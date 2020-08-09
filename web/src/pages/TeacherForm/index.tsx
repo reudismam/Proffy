29c9cb39ff578621c114eece484b576a5cabaa40
reudismam <reudismam@gmail.com>
2020-08-08 23:00:26 -0300

    building teacher-form

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,10 +1,44 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
+import './styles.css';
+import Input from '../../components/Input';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
+import TextArea from '../../components/TextArea';
+
 export default function TeacherForm() {
     return (
-        <div id="page-teacher-list" className="container">
-            <PageHeader title="Que incrível que você quer ministrar aula"/>
+        <div id="page-teacher-form" className="container">
+            <PageHeader title="Que incrível que você quer ministrar aula"
+                description="O primeiro passo é preencher eses formulário de inscrição."
+            />
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+                    <Input name="name" label="Nome completo"/>
+                    <Input name="avatar" label="Avatar" />
+                    <Input name="whatsapp" label="WhatsApp" />
+                    <TextArea name="bio" label="Biografia"/>
+                </fieldset>
+
+                <fieldset>
+                    <legend>Sobre a matéria</legend>
+                    <Input name="subject" label="Matéria"/>
+                    <Input name="cost" label="Custo da sua hora por aula" />
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Avise importante" />
+                        Importante! <br />
+                        Preecha todos os dados
+                    </p>
+                    <button type="button">
+                        Salvar cadastro
+                    </button>
+                </footer>
+            </main>
         </div>
     );
 }
--- web/src/pages/TeacherList/index.jsx
+++ web/src/pages/TeacherList/index.jsx
@@ -4,33 +4,16 @@ import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import Input from '../../components/Input';
 
 export default function TeacherList() {
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis">
                <form action="/" id="search-teachers">
-                   <div className="input-block">
-                       <label htmlFor="subject">Matéria</label>
-                       <input type="text" id="subject">
-
-                       </input>
-                   </div>
-
-                   <div className="input-block">
-                       <label htmlFor="week_day">Dia da semana</label>
-                       <input type="text" id="week_day">
-                           
-                       </input>
-                   </div>
-
-                   <div className="input-block">
-                       <label htmlFor="hora">Hora</label>
-                       <input type="text" id="hora">
-                           
-                       </input>
-                   </div>
-
+                   <Input name="subject" label="Matéria"/>
+                   <Input name="week_day" label="Dia da Semana"/>
+                   <Input name="hora" label="Hora" type="time"/>
                </form>
             </PageHeader>
             <main>
